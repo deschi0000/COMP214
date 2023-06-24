18184f7685ec8c50f70b77e1628e1884fb3d0231
deschi0000 <deschi0000@outlook.com>
2023-06-24 17:25:50 +0800

    update model

--- FinalGroupProjectTest/comp214finalprojecttest/coffees/models.py
+++ FinalGroupProjectTest/comp214finalprojecttest/coffees/models.py
@@ -13,6 +13,7 @@ class Location(models.Model):
     name = models.CharField(max_length=64)
     city = models.CharField(max_length=64)
     country = models.CharField(max_length=64)
+    coffees = models.ManyToManyField(Coffee, blank=True, related_name="coffees")
     
     def __str__(self):
         return f"Name: {self.name}\nCity: {self.city}\nCountry: {self.country}"
@@ -21,6 +22,7 @@ class Employee(models.Model):
     fname = models.CharField(max_length=64)
     lname = models.CharField(max_length=64)
     email = models.CharField(max_length=64)
+    workplace = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="workplace")
 
     def __str__(self):
         return f"First Name: {self.fname}\nLast Name: {self.lname}\nEmail: {self.email}"
